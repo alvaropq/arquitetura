437849d36e0003a2df0d112c313660f86363aba8
Rafael Cruz <rafa08cruz19888@gmail.com>
2021-04-02 21:58:14 -0300

    Segunda parte do trabalho

--- src/main/java/com/mycompany/projet_solo/Correction/Correct.java
+++ src/main/java/com/mycompany/projet_solo/Correction/Correct.java
@@ -1,8 +1,10 @@
 
 package com.mycompany.projet_solo.Correction;
 
+import java.util.Scanner;
+
 public class Correct {
-    public static void main(String[] args) {
+    public static int main(String[] args) {
         Scanner dad = new Scanner(System.in);
 
         Double Scmol, CTCcmol, V_atual;
@@ -35,15 +37,14 @@ public class Correct {
             System.out.println("H + AL ideal 0,0 - atual: ");
             double dado_hal = dad.nextDouble();
 
-
-//=========================================================================================================================
-            //Scomol
+            // =========================================================================================================================
+            // Scomol
             Scmol = (dado_potassio + dado_calcio + dado_magnesio);
             System.out.println("Scmol : " + Scmol);
-            //CTC cmaol
+            // CTC cmaol
             CTCcmol = Scmol + dado_hal;
             System.out.println("CTC cmol: " + CTCcmol);
-            //V% atual
+            // V% atual
             V_atual = 100 * (Scmol / CTCcmol);
             System.out.println("V% atual : " + V_atual);
         }
@@ -70,46 +71,57 @@ public class Correct {
             System.out.println("H + AL ideal 0 - atual: ");
             double dado_hal = dad.nextDouble();
 
-
-//=========================================================================================================================
+            // =========================================================================================================================
             // Scmol
             Scmol = dado_potassio + dado_calcio + dado_magnesio;
             System.out.println("Scmol : " + Scmol);
-            //CTC cmol
+            // CTC cmol
             CTCcmol = Scmol + dado_hal;
             System.out.println("CTC cmol: " + CTCcmol);
-            //V% atual
+            // V% atual
             V_atual = 100 * (Scmol / CTCcmol);
             System.out.println("V% atual : " + V_atual);
 
         }
-//=========================================================================================================================
-        //Calculo de Materia Organica 
+        // =========================================================================================================================
+        // Calculo de Materia Organica
         System.out.println("quantidade de Máteria Organica encontrada :");
         double M_O = dad.nextDouble();
-        //calculo da M.O. %
+        // calculo da M.O. %
         double PorM_O = M_O / 10;
-        System.out.println("Porcentagem M.O : "+ PorM_O);
+        System.out.println("Porcentagem M.O : " + PorM_O);
 
         double Carbono = PorM_O / 1.72 * 10;
         System.out.println("Carbono no solo: " + Carbono);
         System.out.println("TEOR IDEAL DE M.O.    3.0 a 40%");
 
-//=========================================================================================================================
+        // =========================================================================================================================
         // Sistem de plantio
         if (sist == 1) {
             System.out.println("Sistema de Plantio Direto");
         } else if (sist == 2) {
             System.out.println("Sistema de Plantio Convencional");
         }
-//=========================================================================================================================
+        // =========================================================================================================================
         // Comparação da argila
         if (texSolo == 1) {
             System.out.println("+ de 40% Argiloso");
         } else if (texSolo == 2) {
             System.out.println("De 25 a 40% de Argila");
         }
-
+        // |||||||||||||||||||||||||||||||==EXERCICIO
+        // 2==||||||||||||||||||||||||||||||||||||||||||
+        /*int num = 0;
+        System.out.println("IR para análise de fosforo ou voltar");
+        System.out.println("1 para análise ou 2 para voltar");
+        switch (num) {
+        case 1:
+            FosforoPotassio();
+            break;
+        default:
+        }
+        return num;*/
+        return sist;
         
     }
-}
+ }
---
+++ src/test/java/com/mycompany/projet_solo/Correction/Testando.java
@@ -0,0 +1,17 @@
+package com.mycompany.projet_solo.Correction;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author meu pc
+ */
+public class Testando {
+
+    @Test
+    public Testando() {
+    }
+    
+    
+}
