3479c478e517be9c6ab053d1afc33c0e6ede3be8
Rafael Cruz <rafa08cruz19888@gmail.com>
2021-04-11 14:05:35 -0300

    atualização das classes

--- src/main/java/com/mycompany/projet_solo/Correction/PotassioCorrect.java
+++ src/main/java/com/mycompany/projet_solo/Correction/PotassioCorrect.java
@@ -12,6 +12,9 @@ public class PotassioCorrect {
     private double sulfatoPotassio_Magnesio;
 
     public PotassioCorrect(
+
+
+    
         double eficienciaPotassio,
         double potassioAtualDoSolo,
         double potassioDesejado,
@@ -21,17 +24,47 @@ public class PotassioCorrect {
         double cloretoDePotassio,
         double sulfatoPotassio,
         double sulfatoPotassio_Magnesio){
-
-            this.eficienciaPotassio = eficienciaPotassio;
-            this.potassioAtualDoSolo = potassioAtualDoSolo;
-            this.potassioDesejado = potassioDesejado;
-            this.potassioAposCorrecao = potassioAposCorrecao;
-            this.potasioAUsar = potasioAUsar;
-            this.participacaoDoPotassio = participacaoDoPotassio;
-            this.cloretoDePotassio = cloretoDePotassio;
-            this.sulfatoPotassio = sulfatoPotassio;
-            this.sulfatoPotassio_Magnesio = sulfatoPotassio_Magnesio;
         }
+    
+    
+     double ctctotal(double Scmol,double dado_hal){
+        return (Scmol/dado_hal);
+    }
+    double ValorPorToneladaPotassio(
+        double cloretoDePotassio,
+        double sulfatoPotassio,
+        double sulfatoPotassio_Magnesio){
+
+            return (cloretoDePotassio + sulfatoPotassio + sulfatoPotassio_Magnesio);
+        }
+    double TeorAtingir(double potassioDesejado,double ctctotal){
+        return((potassioDesejado * ctctotal)/100) ;
+    }
+
+    double NecessitaPotassio( double potassioAtualDoSolo,double potassioDesejado){
+        return (potassioAtualDoSolo - potassioDesejado);
+    }
+    double Aproveita(double dado_potassio,double eficienciaPotassio){
+        return ((dado_potassio * 391 * 2 * 1.2)/eficienciaPotassio);
+    }
+    double QntPotassioHectare(double Aproveita,double potasioAUsar){
+        return ((Aproveita * 100)/potasioAUsar);
+    }
+    double QntPotassioAlquere(double QntPotassioHectare){
+        return (QntPotassioHectare * 2.49);
+    }
+    double CustoAlquere(double ValorPorToneladaPotassio,double QntPotassioAlquere){
+        return ((QntPotassioAlquere/100) * ValorPorToneladaPotassio);
+    }
+    double CustoHectare(double CustoAlquere){
+        return (CustoAlquere/2.42);
+    }
+    double CustoTotal(double CustoHectare,double area_talhao){
+        return (CustoHectare * area_talhao);
+    }
+
+
+
         public double getEficiencia(){
             return eficienciaPotassio;
         }
